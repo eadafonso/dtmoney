1a19c908d9fd4b7d609456ffa57bf1348948e43e
Edvaldo Afonso <eadafonso@gmail.com>
2022-02-06 13:12:54 +0100

    feat: create componente select type

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import Modal from "react-modal";
 
 import closeImg from "../../assets/close.svg";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
 
-import { Container } from "./styles";
+import { Container, RadioBox, TransactionTypeContainer } from "./styles";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -13,6 +16,8 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTransactionModalProps) {
+  const [type, setType] = useState("deposit");
+
   return (
     <Modal
       isOpen={isOpen}
@@ -33,6 +38,34 @@ export function NewTransactionModal({
         <input type="text" placeholder="Título" />
 
         <input type="number" placeholder="Valor" />
+
+        <TransactionTypeContainer>
+          <RadioBox
+            type="button"
+            onClick={() => {
+              setType("deposit");
+            }}
+            isActive={type === "deposit"}
+            activeColor="green"
+          >
+            <img src={incomeImg} alt="Entradas" />
+
+            <span>Entradas</span>
+          </RadioBox>
+
+          <RadioBox
+            type="button"
+            onClick={() => {
+              setType("withdraw");
+            }}
+            isActive={type === "withdraw"}
+            activeColor="red"
+          >
+            <img src={outcomeImg} alt="Saídas" />
+
+            <span>Saídas</span>
+          </RadioBox>
+        </TransactionTypeContainer>
 
         <input type="text" placeholder="Categoria" />
 
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { darken, transparentize } from "polished";
 
 export const Container = styled.form`
   h2 {
@@ -10,7 +11,7 @@ export const Container = styled.form`
   input {
     width: 100%;
     padding: 0 1.5rem;
-    height: 3rem;
+    height: 3.5rem;
     border-radius: 0.24rem;
     background: #e7e9ee;
     border: 1px solid #d7d7d7;
@@ -29,7 +30,7 @@ export const Container = styled.form`
   button[type="submit"] {
     width: 100%;
     padding: 0 1.5rem;
-    height: 3rem;
+    height: 3.5rem;
     background: var(--green);
     color: #fff;
     border-radius: 0.25rem;
@@ -41,5 +42,51 @@ export const Container = styled.form`
     &:hover {
       filter: brightness(0.9);
     }
+  }
+`;
+
+export const TransactionTypeContainer = styled.div`
+  margin: 1rem 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 0.5rem;
+`;
+
+interface RadioBoxProps {
+  isActive: boolean;
+  activeColor: "green" | "red";
+}
+
+const colors = {
+  green: "#33cc95",
+  red: "#e52e4d",
+};
+
+export const RadioBox = styled.button<RadioBoxProps>`
+  height: 3.5rem;
+  border: 1px solid #d7d7d7;
+  border-radius: 0.25rem;
+  background: ${(props) =>
+    props.isActive
+      ? transparentize(0.9, colors[props.activeColor])
+      : "transparent"};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: border-color 0.2s;
+
+  &:hover {
+    border-color: ${darken(0.1, "#d7d7d7")};
+  }
+
+  img {
+    width: 20px;
+    height: 20px;
+  }
+
+  span {
+    display: inline-block;
+    margin-left: 1rem;
+    color: var(--text-title);
   }
 `;
